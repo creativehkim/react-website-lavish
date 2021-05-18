7ea60bae703216da39c3b0609bf765ee142e6284
Hyunji Kim <creative.hkim@gmail.com>
2021-05-17 23:12:24 -0400

    hero section added

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,11 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDescription: true,
+  topLine: 'Exclusive Access',
+  headline: 'Unlimited Transactions with zero fees',
+  description: 'Get access to our exclusive diamond card that allows you to send unliminted transactions without getting charged any fees',
+  imgStart: '',
+  img: 'images/svg-1.svg',
+  alt: 'Credit Card'
+}
